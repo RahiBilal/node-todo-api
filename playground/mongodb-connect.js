aece41521718e6edd4d8999395851d5bbfc25ce5
Rahi Bilal <ansaribilal.5161@gmail.com>
2019-03-09 22:49:35 +0530

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,24 @@
+//const MongoClient= require('mongodb').MongoClient;
+const {MongoClient, ObjectID}= require('mongodb');
+
+//Connect to mongodb
+MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', {useNewUrlParser: true}, (error, client)=> {
+    if(error){
+        return console.log(error);
+    }
+    console.log('Connected to mongodb server.');
+    const db= client.db('TodoApp');
+    db.collection('users').insertOne({
+        name: 'Rahi Bilal',
+        age: 23,
+        location: 'Aligarh'
+    }, (error, result)=>{
+        if(error){
+            return console.log('Unable to insert', error);
+        }
+        console.log(result.ops[0]._id.getTimestamp());
+    });
+
+    client.close();
+    
+});
